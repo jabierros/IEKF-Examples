f04be7e67165538c12a673ed0ebcda6eabf339f6
jabierros <jros@unavarra.es>
2021-06-02 20:45:06 +0200

    removing unneeded libIEKF files from other examples

--- Segway_2D/Segway_Gyro_and_Accel/get_u.m
+++
@@ -1,8 +0,0 @@
-function u_meas=get_u()
-global t
-global u_actual n_u
-global Sigma_u_actual u_actual_func 
-set_x_actual();
-u_actual=u_actual_func(t);
-u_meas=u_actual+Sigma_u_actual*randn(n_u,1);
-end
